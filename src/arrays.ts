c6f7abebb2e2d922c0ef4b407ae936b9d730f333
Katlego Matsipane <katlegom@udel.edu>
2026-02-23 21:53:15 -0500

    Completed injectPositive function

--- src/arrays.ts
+++ src/arrays.ts
@@ -122,5 +122,26 @@ export function makeMath(addends: number[]): string {
  * And the array [1, 9, 7] would become [1, 9, 7, 17]
  */
 export function injectPositive(values: number[]): number[] {
-    return [];
+    if (values.length === 0) {
+        return [0];
+    }
+    const result: number[] = [...values];
+    let sum: number = 0;
+    let negative: number = values.findIndex((neg: number): boolean => neg < 0);
+    let array: number[] = values.slice(0, negative);
+    if (negative >= 0) {
+        sum = array.reduce(
+            (currentTotal: number, num: number) => currentTotal + num,
+            0,
+        );
+        result.splice(negative + 1, 0, sum);
+    } else {
+        sum = result.reduce(
+            (currentTotal: number, num: number) => currentTotal + num,
+            0,
+        );
+        result.push(sum);
+    }
+
+    return result;
 }
