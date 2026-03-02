9f22d3f681fe7cb6e9a07a1b7e61b4f494990bcc
Katlego Matsipane <katlegom@udel.edu>
2026-03-01 20:42:03 -0500

    Completed duplicateQuestion Function

--- src/objects.ts
+++ src/objects.ts
@@ -114,7 +114,12 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    return {
+        ...oldQuestion,
+        published: false,
+        name: "Copy of " + oldQuestion.name,
+        id: id,
+    };
 }
 
 /**
