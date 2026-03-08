709eb20c59c94796d7539ba2f085260da6228881
Katlego Matsipane <katlegom@udel.edu>
2026-03-07 20:28:39 -0500

    Completed StartAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,34 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [quiz, setQuiz] = useState<boolean>(false);
+    const [attempt, setAttempt] = useState<number>(4);
+    function changeQuiz(): void {
+        setQuiz(!quiz);
+    }
+    function changeAttemptsQuiz(): void {
+        setAttempt(attempt - 1);
+        setQuiz(!quiz);
+    }
+
+    function changeAttemptsMulligan(): void {
+        setAttempt(attempt + 1);
+    }
+    return (
+        <div>
+            <Button
+                onClick={changeAttemptsQuiz}
+                disabled={quiz || attempt <= 0}
+            >
+                Start Quiz
+            </Button>
+            <Button onClick={changeQuiz} disabled={!quiz}>
+                Stop Quiz
+            </Button>
+            <Button onClick={changeAttemptsMulligan} disabled={quiz}>
+                Mulligan
+            </Button>
+            {<div>Attempts: {attempt}</div>}
+        </div>
+    );
 }
