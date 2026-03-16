cdc0d85ac36b99277a872cc6c2cf8696040452c1
Katlego Matsipane <katlegom@udel.edu>
2026-03-16 11:27:15 -0400

    Completed GiveAttempts

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
 import { CheckAnswer } from "./form-components/CheckAnswer";
 import { GiveAttempts } from "./form-components/GiveAttempts";
-import { EditMode } from "./form-components/EditMode";
-import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
-import { ChangeColor } from "./form-components/ChangeColor";
+// import { EditMode } from "./form-components/EditMode";
+// import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
+// import { ChangeColor } from "./form-components/ChangeColor";
 
 function App(): React.JSX.Element {
     return (
@@ -28,14 +28,14 @@ function App(): React.JSX.Element {
             <hr></hr>
             <GiveAttempts></GiveAttempts>
             <hr></hr>
-            <EditMode></EditMode>
+            {/* <EditMode></EditMode> */}
             <hr></hr>
-            <ChangeColor></ChangeColor>
+            {/* <ChangeColor></ChangeColor> */}
             <hr></hr>
-            <MultipleChoiceQuestion
+            {/* <MultipleChoiceQuestion
                 options={["a", "b", "c"]}
                 expectedAnswer="b"
-            ></MultipleChoiceQuestion>
+            ></MultipleChoiceQuestion> */}
             <hr></hr>
             <DoubleHalf></DoubleHalf>
             <hr></hr>
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,40 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
+    const [rAttempts, rSetAttempts] = useState<string>("0");
+    function decreaseAttempts() {
+        setAttempts(attempts - 1);
+    }
+    function increaseAttempts() {
+        const requestedAttempts = parseInt(rAttempts) || 0;
+        setAttempts(attempts + requestedAttempts);
+        rSetAttempts("0");
+    }
+
+    function updateAttempts(event: React.ChangeEvent<HTMLInputElement>) {
+        rSetAttempts(event.target.value);
+    }
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            Attempts Remaining = {attempts}
+            <div>
+                <Button onClick={decreaseAttempts} disabled={attempts <= 0}>
+                    Use
+                </Button>
+            </div>
+            <Form.Group controlId="formAttempt">
+                <Form.Label>:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={rAttempts}
+                    onChange={updateAttempts}
+                />
+            </Form.Group>
+            <Button onClick={increaseAttempts}> Gain </Button>
         </div>
     );
 }
