fd593976f63cb594448dc070c5881042355648ac
Katlego Matsipane <katlegom@udel.edu>
2026-03-16 11:42:02 -0400

    Completed EditMode

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
 import { CheckAnswer } from "./form-components/CheckAnswer";
 import { GiveAttempts } from "./form-components/GiveAttempts";
-// import { EditMode } from "./form-components/EditMode";
+import { EditMode } from "./form-components/EditMode";
 // import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
 // import { ChangeColor } from "./form-components/ChangeColor";
 
@@ -28,7 +28,7 @@ function App(): React.JSX.Element {
             <hr></hr>
             <GiveAttempts></GiveAttempts>
             <hr></hr>
-            {/* <EditMode></EditMode> */}
+            <EditMode></EditMode>
             <hr></hr>
             {/* <ChangeColor></ChangeColor> */}
             <hr></hr>
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,49 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [isEditMode, setIsEditMode] = useState<boolean>(false);
+    const [userName, setUserName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+    function updateEditMode() {
+        setIsEditMode(!isEditMode);
+    }
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Switch
+                type="switch"
+                id="edit-mode-switch"
+                checked={isEditMode}
+                onChange={updateEditMode}
+            />
+            {!isEditMode ?
+                <div>
+                    {userName} is {isStudent ? "a student" : "not a student"}
+                </div>
+            :   <div>
+                    <Form.Group controlId="formUserName">
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={userName}
+                            onChange={(e) => {
+                                setUserName(e.target.value);
+                            }}
+                        />
+                    </Form.Group>
+                    <Form.Check
+                        type="checkbox"
+                        id="is-student-checkbox"
+                        label="Student?"
+                        checked={isStudent}
+                        onChange={(e) => {
+                            setIsStudent(e.target.checked);
+                        }}
+                    />
+                </div>
+            }
         </div>
     );
 }
