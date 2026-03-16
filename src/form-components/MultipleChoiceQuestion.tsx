25e42e1b1134232ecacf4c49e4e9108a340631aa
Katlego Matsipane <katlegom@udel.edu>
2026-03-16 12:11:13 -0400

    Completed MultipleChoiceQuestion

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,27 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    // This is the State (Model)
+    const [answer, setAnswer] = useState<string>(options[0]);
+
+    // This is the Control
+    function updateAnswer(event: React.ChangeEvent<HTMLSelectElement>) {
+        setAnswer(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="multipleChoice">
+                <Form.Label>What is the correct answer?</Form.Label>
+                <Form.Select value={answer} onChange={updateAnswer}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {expectedAnswer === answer ? "✔️" : "❌"}
         </div>
     );
 }
